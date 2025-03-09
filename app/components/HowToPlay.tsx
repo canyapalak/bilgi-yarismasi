bd3807083f484c2843dcf752feac3533913310aa
canyapalak <canyapalak@gmail.com>
2025-03-09 21:06:13 +0100

    update databank for questions

--- app/components/HowToPlay.tsx
+++ app/components/HowToPlay.tsx
@@ -4,11 +4,24 @@ export default function HowToPlay({ closeHowToPlay }: HowToPlayProps) {
   return (
     <div className="flex flex-col gap-8 items-center">
       <p className="text-neutral-700 fade-in break-words text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
+        Bu oyun, hem bilginizi test etmek hem de süper kahraman dünyasında
+        yerinizi almanız için tasarlandı!
+        <br />
+        <br />
+        Önce{" "}
+        <span className="text-brick-default">istediğin kategoriyi seç</span> ve
+        bilgi yarışmasına başla. Her soru için{" "}
+        <span className="text-brick-default">20 saniyen</span> var! Ama rahat
+        modda süre sınırı yok, dilediğin kadar düşünebilirsin. Toplam{" "}
+        <span className="text-brick-default">10 soru</span> cevapladıktan sonra,
+        aldığın puana göre{" "}
+        <span className="text-brick-default">
+          bir süper kahramanla eşleşeceksin!
+        </span>
+        <br />
+        <br />
+        Bakalım bilgi birikimin hangi kahramana daha yakın? Şimdi sıra sende!
+        Bilginin gücünü göster ve kahramanını keşfet!
       </p>
       <div
         className="button-prm bg-gray-default text-neutral-50 text-2xl rounded-md p-3
